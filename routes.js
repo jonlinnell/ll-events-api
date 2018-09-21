cc1656d19f65ade16a9d48c7afeeb31b65a712e0
Jon Linnell <linnell.jon@gmail.com>
2018-09-21 06:25:27 +0100

    getRailDepartureBoard added

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const { fetchEvents } = require('./lib/events')
 const fetchNews = require('./lib/news')
 const getBusDepartures = require('./lib/getBusDepartures')
+const getRailDepartureBoard = require('./lib/getRailDepartureBoard')
 
 const routes = (app) => {
   app.get('/events', (req, res) => {
@@ -21,6 +22,17 @@ const routes = (app) => {
     getBusDepartures(stopCode)
       .then(data => res.json(data))
       .catch(error => res.status(500).send(`An error occurred loading the bus data: ${error}`))
+  })
+
+  app.get('/rail/:station/:destination?', (req, res) => {
+    const { station, destination } = req.params
+
+    getRailDepartureBoard({
+      station,
+      destination,
+    })
+      .then(data => res.json(data))
+      .catch(error => res.status(500).send(error))
   })
 }
 
