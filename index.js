88270406d3daa06e4a5d1b7931bb867c4131bc7a
Jon Linnell <linnell.jon@gmail.com>
2018-09-25 13:54:31 +0100

    Add CORS for backend

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 require('dotenv').config()
 
 const app = express()
 
 app.use(bodyParser.json())
+app.use(cors())
 
 const port = process.env.PORT || 3000
 
