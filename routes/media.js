8432e35b1941ae252a5d50f91e219cfe3c9a6896
Jon Linnell <linnell.jon@gmail.com>
2018-11-29 21:04:18 +0000

    Error handling tweaks

--- routes/media.js
+++ routes/media.js
@@ -6,13 +6,13 @@ const routes = (app) => {
   app.get(`${PREFIX}/active`, (req, res) => {
     getActiveMedia()
       .then(data => res.json(data))
-      .catch(error => res.status(500).send(error))
+      .catch(error => res.status(500).send(`Error: ${error}`))
   })
 
   app.post(`${PREFIX}/add`, (req, res) => {
     addMedia(req)
       .then(data => res.json(data))
-      .catch(error => res.status(500).send(error))
+      .catch(error => res.status(500).send(`Error: ${error}`))
   })
 }
 
